e4440d297074407fc9c34ad340e2b8e154978809
Robert E. O. Speedwagon <ellmaxpro1@gmail.com>
2026-08-26 21:10:20 +0300

    style: run cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
+use reqwest::StatusCode;
 use std::process::exit;
 use std::{fs::File, io::Write};
 use thiserror::Error;
-use reqwest::StatusCode;
 
-use reqwest::{Client, Response, header::{CONTENT_DISPOSITION}};
-use futures_util::{StreamExt};
+use futures_util::StreamExt;
+use reqwest::{Client, Response, header::CONTENT_DISPOSITION};
 
 #[derive(Error, Debug)]
 enum DownloadError {
     #[error("Network error: {0}")]
     Network(#[from] reqwest::Error),
-    
+
     #[error("IO error: {0}")]
     IO(#[from] std::io::Error),
 
@@ -33,28 +33,35 @@ async fn main() {
         exit(2)
     }
 
-    let d : Result<(), DownloadError> = download(&args[1]).await;
+    let d: Result<(), DownloadError> = download(&args[1]).await;
 
     match d {
         Ok(..) => println!("ok"),
-        Err(e) => {eprintln!("{}", e); exit(1)}
+        Err(e) => {
+            eprintln!("{}", e);
+            exit(1)
+        }
     }
 }
 
-async fn download(url: &str) -> Result<(), DownloadError>{
+async fn download(url: &str) -> Result<(), DownloadError> {
     let client: Client = Client::new();
     let response: Response = client.get(url).send().await?;
-    
-    if !response.status().is_success(){
+
+    if !response.status().is_success() {
         return Err(DownloadError::HttpStatus(response.status()));
     }
 
-    let header = response.headers()
+    let header = response
+        .headers()
         .get(CONTENT_DISPOSITION)
         .and_then(|val| val.to_str().ok())
         .ok_or(DownloadError::InvalidContentDisposition)?;
-    
-    let filename = header.split("filename=").nth(1).ok_or(DownloadError::MissingFilename)?;
+
+    let filename = header
+        .split("filename=")
+        .nth(1)
+        .ok_or(DownloadError::MissingFilename)?;
 
     // let size = response.content_length().ok_or_else(|| anyhow!("Ошибка("))?;
 
@@ -65,6 +72,6 @@ async fn download(url: &str) -> Result<(), DownloadError>{
     while let Some(chunk) = stream.next().await {
         let chunk = chunk?;
         file.write_all(&chunk)?;
-    };
+    }
     Ok(())
 }
