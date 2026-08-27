955e3d8e064055cb4bc6193c80fc57b6935629fa
Robert E. O. Speedwagon <109958355+Ellmax@users.noreply.github.com>
2026-08-27 03:12:43 +0300

    Feature/parallel download (#2)
    * feat: parallel chunked download with resume support
    
    * refactor: improve header handling and add User-Agent
    
    * feat: add clap CLI argument parser with --parts option

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,23 @@
-use reqwest::StatusCode;
+use reqwest::{
+    StatusCode,
+    header::{ACCEPT_RANGES, CONTENT_LENGTH},
+};
+use std::path::PathBuf;
 use std::process::exit;
-use std::{fs::File, io::Write};
 use thiserror::Error;
 
-use futures_util::StreamExt;
-use reqwest::{Client, Response, header::CONTENT_DISPOSITION};
+use futures_util::{StreamExt, stream};
+use reqwest::{
+    Client,
+    header::{CONTENT_DISPOSITION, RANGE},
+};
+use tokio::fs::File;
+use tokio::fs::OpenOptions;
+use tokio::io::{AsyncSeekExt, AsyncWriteExt};
+
+use content_disposition::parse_content_disposition;
+
+use clap::Parser;
 
 #[derive(Error, Debug)]
 enum DownloadError {
@@ -14,26 +27,29 @@ enum DownloadError {
     #[error("IO error: {0}")]
     IO(#[from] std::io::Error),
 
-    #[error("Invalid Content-Disposition header")]
-    InvalidContentDisposition,
-
-    #[error("No filename found in URL or headers")]
-    MissingFilename,
-
     #[error("HTTP error: {0}")]
     HttpStatus(StatusCode),
 }
 
+struct Part {
+    start: u64,
+    end: Option<u64>,
+    temp_path: PathBuf,
+}
+#[derive(Parser)]
+#[command(version)]
+struct Args {
+    #[arg(short, long, default_value_t = 1)]
+    parts: usize,
+
+    url: String,
+}
+
 #[tokio::main]
 async fn main() {
-    let args: Vec<String> = std::env::args().collect();
+    let args = Args::parse();
 
-    if args.len() != 2 {
-        eprintln!("Usage: dlr <url>");
-        exit(2)
-    }
-
-    let d: Result<(), DownloadError> = download(&args[1]).await;
+    let d: Result<(), DownloadError> = download(&args.url, args.parts).await;
 
     match d {
         Ok(..) => println!("ok"),
@@ -44,34 +60,127 @@ async fn main() {
     }
 }
 
-async fn download(url: &str) -> Result<(), DownloadError> {
-    let client: Client = Client::new();
-    let response: Response = client.get(url).send().await?;
+async fn download(url: &str, num_parts: usize) -> Result<(), DownloadError> {
+    let client = reqwest::Client::builder().user_agent("dlr/0.1.0").build()?;
+    let head_response = client.head(url).send().await?;
+
+    if !head_response.status().is_success() {
+        return Err(DownloadError::HttpStatus(head_response.status()));
+    }
+
+    let size = head_response.headers().get(CONTENT_LENGTH);
+
+    let header = head_response
+        .headers()
+        .get(CONTENT_DISPOSITION)
+        .and_then(|v| v.to_str().ok());
+
+    let mut filename: Option<String> = if let Some(header) = header {
+        parse_content_disposition(header).filename_full()
+    } else {
+        None
+    };
+
+    if filename.is_none() {
+        filename = head_response
+            .url()
+            .path_segments()
+            .and_then(|segments| segments.last())
+            .map(|s| s.into())
+    }
+
+    let filename = filename.unwrap_or_else(|| "downloaded_file".into());
+
+    let accept_ranges = head_response
+        .headers()
+        .get(ACCEPT_RANGES)
+        .and_then(|v| v.to_str().ok())
+        .map(|s| s.contains("bytes"))
+        .unwrap_or(false);
+
+    let use_parralel = size.is_some() && accept_ranges && num_parts > 1;
+
+    let parts: Vec<Part> = if use_parralel {
+        let size: u64 = size.unwrap().to_str().unwrap().parse().unwrap();
+        let part_size = size / num_parts as u64;
+        (0..num_parts)
+            .map(|i| {
+                let start = i as u64 * part_size;
+                let end = if i == num_parts - 1 {
+                    Some(size - 1)
+                } else {
+                    Some(start + part_size - 1)
+                };
+                let temp_path = format!("{}.part{}", filename, i).into();
+                Part {
+                    start,
+                    end,
+                    temp_path,
+                }
+            })
+            .collect::<Vec<Part>>()
+    } else {
+        vec![Part {
+            start: 0,
+            end: None,
+            temp_path: format!("{}.part0", filename).into(),
+        }]
+    };
+
+    let concurrency: usize = if use_parralel { num_parts } else { 1 };
+    let part_futures = parts.iter().map(|part| download_part(&client, url, part));
+
+    let results: Vec<Result<(), DownloadError>> = stream::iter(part_futures)
+        .buffer_unordered(concurrency)
+        .collect()
+        .await;
+
+    for result in results {
+        result?
+    }
+
+    let mut final_file = File::create(&filename).await?;
+    for part in parts {
+        let mut part_file = File::open(&part.temp_path).await?;
+        tokio::io::copy(&mut part_file, &mut final_file).await?;
+        tokio::fs::remove_file(&part.temp_path).await?;
+    }
+
+    Ok(())
+}
+
+async fn download_part(client: &Client, url: &str, part: &Part) -> Result<(), DownloadError> {
+    let mut file = OpenOptions::new()
+        .write(true)
+        .read(true)
+        .create(true)
+        .open(&part.temp_path)
+        .await?;
+
+    let size = file.metadata().await?.len();
+    let start = part.start + size;
+
+    let mut request = client.get(url);
+
+    if let Some(end) = part.end {
+        request = request.header(RANGE, format!("bytes={}-{}", start, end))
+    } else if size > 0 {
+        file.set_len(0).await?;
+    }
+
+    let response = request.send().await?;
 
     if !response.status().is_success() {
         return Err(DownloadError::HttpStatus(response.status()));
     }
 
-    let header = response
-        .headers()
-        .get(CONTENT_DISPOSITION)
-        .and_then(|val| val.to_str().ok())
-        .ok_or(DownloadError::InvalidContentDisposition)?;
-
-    let filename = header
-        .split("filename=")
-        .nth(1)
-        .ok_or(DownloadError::MissingFilename)?;
-
-    // let size = response.content_length().ok_or_else(|| anyhow!("Ошибка("))?;
-
-    let mut file = File::create(filename)?;
+    file.seek(std::io::SeekFrom::Start(size)).await?;
 
     let mut stream = response.bytes_stream();
-
     while let Some(chunk) = stream.next().await {
         let chunk = chunk?;
-        file.write_all(&chunk)?;
+        file.write_all(&chunk).await?;
     }
+
     Ok(())
 }
